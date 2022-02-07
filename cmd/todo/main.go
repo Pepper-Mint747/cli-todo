3ad1e62f3f0742a716d80cf7a1fe2875f44f9a8b
Brian Omoga <omogabrian2060@outlook.com>
2022-02-07 11:59:02 +0300

    added ability to handle multiple command-line options

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/Pepper-Mint747/goproj/interacting/todo"
 	"os"
-	"strings"
 )
 
 //Hard-coding the file name
 const todoFileName = ".todo.json"
 
 func main() {
+
+	//Parsing command line flags
+	task := flag.String("task", "", "Task to be included in the ToDo list")
+	list := flag.Bool("list", false, "List all tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+
+	flag.Parse()
+
+	//Define an item list
 	l := &todo.List{}
 
 	//Use the Get method to read to do items from file
@@ -21,27 +30,40 @@ func main() {
 
 	// Decide what to do based on the number of arguments provided
 	switch {
-	// For no extra arguments, print the list
-	case len(os.Args) == 1:
+	case *list:
 		// List current to do items
 		for _, item := range *l {
-			fmt.Println(item.Task)
+			if !item.Done {
+				fmt.Println(item.Task)
+			}
 		}
 
-	// Concatenate all provided arguments with a space and
-	//add to the list as an item
-	default:
+	case *complete > 0:
+		//Complete the given item
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 
-		//Concatenate all arguments with a space
-		item := strings.Join(os.Args[1:], " ")
-
-		//Add the task
-		l.Add(item)
-
-		//Save the new List
+		// Save the new list
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+
+	case *task != "":
+		//Add the task
+		l.Add(*task)
+
+		// Save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+	default:
+		//Invalid flag provided
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
 	}
 }
