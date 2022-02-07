6334a13372e8152716c20d3b5cb46ab715864c80
Brian Omoga <omogabrian2060@outlook.com>
2022-02-08 00:54:29 +0300

    added increased flexibility with env variables for storage os tasks

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -8,8 +8,8 @@ import (
 	"github.com/Pepper-Mint747/goproj/interacting/todo"
 )
 
-//Hard-coding the file name
-const todoFileName = ".todo.json"
+//Default file name
+var todoFileName = ".todo.json"
 
 func main() {
 
@@ -27,6 +27,11 @@ func main() {
 	complete := flag.Int("complete", 0, "Item to be completed")
 
 	flag.Parse()
+
+	// Check if the user defined the ENV VAR for custom file name
+	if os.Getenv("TODO_FILENAME") != "" {
+		todoFileName = os.Getenv("TODO_FILENAME")
+	}
 
 	//Define an item list
 	l := &todo.List{}
